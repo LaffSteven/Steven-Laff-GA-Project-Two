e6792b09d26721ecf0d20c2e355fb2c7f5b52314
LaffSteven <steve.laff@gmail.com>
2022-05-12 17:35:11 -0400

    card schema created

---
+++ models/card.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema({
+    name: {type:String, required:true},
+    manaCost: {
+        red: Number,
+        green: Number,
+        blue: Number,
+        white: Number,
+        black: Number,
+        colorless: Number,
+        x: Boolean
+    },
+    img: {type:String, required:true},
+    typeLine: {
+        type: {type:String, required:true},
+        subTypes: [String]
+    },
+    text: String,
+    stats: {
+        power: Number,
+        toughness: Number,
+        loyalty: Number
+    },
+    misc: {
+        expansion: String,
+        illustrator: String,
+        collectorNum: Number
+    }
+
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
